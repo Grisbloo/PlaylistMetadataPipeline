9668c29f8532675b0f362e4f39c97ab7e98ff116
Victor Runyan <vsr200001@utdallas.edu>
2026-06-25 02:41:01 -0500

    browser_factory.py is finished as well, thsi allows the easy use to simply grab a fresh browser whenever needed to support main.py

---
+++ browser_factory.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+class BrowserFactory:
+    #This prevents python from making the function about itself and makes it a tool to be used
+    @staticmethod
+    def create_driver(is_headless=True):
+        chrome_options = Options()
+        if is_headless:
+            #This prevents a ui from appearing when using the browser
+            chrome_options.add_argument("--headless")
+            #This prevents the gpu from makign the webpage draw faster when using the browser
+            chrome_options.add_argument("--disable-gpu")
+            #Allow chrome to run normally when using the browser
+            chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(options = chrome_options)
+        return driver
