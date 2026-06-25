aea9f2078a896392fac3bf4ea0d16a12f9c4d8aa
Victor Runyan <vsr200001@utdallas.edu>
2026-06-25 01:40:00 -0500

    Initial commit with SQL page

---
+++ DataBase.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Log(
+
+SongId TEXT PRIMARY KEY,
+
+Title TEXT Not Null,
+
+Artist TEXT Not Null,
+
+Album TEXT Not Null,
+
+Year INTEGER,
+
+Duration INTEGER Not Null,
+
+Downloaded BOOLEAN
+
+) 
+
